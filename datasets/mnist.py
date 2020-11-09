2841a6004cd1fa14756ca1b471c71361cd0d058c
tflahaul <thomas.pro@tutanota.com>
2020-11-09 19:30:06 +0100

    python

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -6,24 +6,27 @@
 #    By: thflahau <thflahau@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/05 19:50:33 by thflahau          #+#    #+#              #
-#    Updated: 2020/11/06 15:21:09 by thflahau         ###   ########.fr        #
+#    Updated: 2020/11/09 19:29:20 by thflahau         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 """ Import the MNIST dataset from internet """
 
-from module.Network import Network
+import requests, gzip
 
-def save_file(name, data):
+URL = 'http://yann.lecun.com/exdb/mnist/'
+
+def save_file(name : str, data : bytes):
 	with open(name, 'wb') as file:
 		file.write(data)
 
-def main():
-	network = Network('http://yann.lecun.com/exdb/mnist/')
-	save_file('mnist_images_train.bin', network.get('train-images-idx3-ubyte.gz', unzip=True))
-	save_file('mnist_labels_train.bin', network.get('train-labels-idx1-ubyte.gz', unzip=True))
-	save_file('mnist_images_test.bin', network.get('t10k-images-idx3-ubyte.gz', unzip=True))
-	save_file('mnist_labels_test.bin', network.get('t10k-labels-idx1-ubyte.gz', unzip=True))
+def get(output : str, file):
+	print(f'Fetching data from {URL + file}...')
+	response = requests.get(URL + file)
+	save_file(output, gzip.decompress(response.content))
 
 if __name__ == '__main__':
-	main()
+	get('mnist_images_train.bin','train-images-idx3-ubyte.gz')
+	get('mnist_labels_train.bin','train-labels-idx1-ubyte.gz')
+	get('mnist_images_test.bin', 't10k-images-idx3-ubyte.gz')
+	get('mnist_labels_test.bin', 't10k-labels-idx1-ubyte.gz')
